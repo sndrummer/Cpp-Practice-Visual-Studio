798750d16cc7d176643a1964db03e11f73a133b9
Samuel Nuttall <sndrummer@gmail.com>
2019-10-14 14:31:28 -0600

    lambda example added

--- Lambdas-and-more/src/main.cpp
+++ Lambdas-and-more/src/main.cpp
@@ -1,9 +1,11 @@
 #include "functionPointers.h"
 #include "parameterPacks.h"
+#include "lambdas.h"
 
 int main()
 {
-	runPointerExample();
-	runParameterPackExample();
+	// runPointerExample();
+	// runParameterPackExample();
+	runLambdaTest();
 	return 0;
 }
