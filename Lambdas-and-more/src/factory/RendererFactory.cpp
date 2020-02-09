d953ba448cf781e6886c78f3f11706ca2946b64b
Samuel Nuttall <sndrummer@gmail.com>
2020-02-09 15:56:49 -0700

    removed cpp file

--- Lambdas-and-more/src/factory/RendererFactory.cpp
+++ Lambdas-and-more/src/factory/RendererFactory.cpp
@@ -1,1 +1,31 @@
 #include "RendererFactory.h"
+
+
+RendererFactory& RendererFactory::getInstance()
+{
+	static RendererFactory factory;
+	return factory;
+}
+
+
+void RendererFactory::registerRenderer(const std::string& type, const CreateCallback cb)
+{
+	mRenderers[type] = cb;
+}
+
+//mRenderers[type] = renderer;
+void RendererFactory::unregisterRenderer(const std::string& type)
+{
+	mRenderers.erase(type);
+}
+
+IRenderer* RendererFactory::createRenderer(const std::string& type)
+{
+	auto it = mRenderers.find(type);
+
+	if (it != mRenderers.end())
+	{
+		// call the creation callback
+		return it->second();
+	}
+}
--- Lambdas-and-more/src/factory/RendererFactory.h
+++ Lambdas-and-more/src/factory/RendererFactory.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <map>
+#include <string>
+#include "IRenderer.h"
+
 class RendererFactory
 {
-};
+public:
+	static RendererFactory& getInstance(); // make it a singleton
 
+	typedef IRenderer* (*CreateCallback)(); // callback function constructor  
+	void registerRenderer(const std::string& type, CreateCallback cb);
+	void unregisterRenderer(const std::string& type);
+	IRenderer* createRenderer(const std::string& type);
+
+
+private:
+	// Make a singleton
+	RendererFactory() = default;
+	~RendererFactory() = default;
+
+	typedef std::map<std::string, CreateCallback> RendererMap;
+	RendererMap mRenderers;
+};
